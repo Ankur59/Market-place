5c8a19ccf1d9d9b31ee5961301db3503f6cd25c5
Ankur <ankur.kumar.adtu@gmail.com>
2025-02-12 16:30:49 +0530

    tailwind working

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,14 @@
 import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaView>
+      <View>
+        <Text className="text-yellow-500">
+          Welcome to my app!
+        </Text>
+      </View>
+    </SafeAreaView>
   );
 }
